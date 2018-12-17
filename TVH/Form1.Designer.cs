c3570f1f63af8621f523c4dfa5444a6f6cb5621f
Unknown <postvoorkayle@hotmail.com>
2018-12-17 12:50:12 +0100

    Machtverhaffing en Forloop groot deel af

--- TVH/Form1.Designer.cs
+++ TVH/Form1.Designer.cs
@@ -29,47 +29,44 @@
         private void InitializeComponent()
         {
             this.goButton = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timeBox = new System.Windows.Forms.GroupBox();
-            this.newInput = new System.Windows.Forms.Button();
-            this.yearCount = new System.Windows.Forms.Label();
-            this.dayCount = new System.Windows.Forms.Label();
-            this.minuteCount = new System.Windows.Forms.Label();
-            this.secondCount = new System.Windows.Forms.Label();
             this.mileniaCount = new System.Windows.Forms.Label();
+            this.secondCount = new System.Windows.Forms.Label();
+            this.minuteCount = new System.Windows.Forms.Label();
+            this.dayCount = new System.Windows.Forms.Label();
+            this.yearCount = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.forloopButton = new System.Windows.Forms.Button();
+            this.recursieButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.timeBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // goButton
             // 
-            this.goButton.Location = new System.Drawing.Point(180, 79);
+            this.goButton.Location = new System.Drawing.Point(12, 63);
             this.goButton.Name = "goButton";
             this.goButton.Size = new System.Drawing.Size(82, 23);
             this.goButton.TabIndex = 0;
-            this.goButton.Text = "Go!";
+            this.goButton.Text = "Macht";
             this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
             // 
-            // textBox2
+            // inputTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(268, 80);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(162, 22);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(12, 80);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(162, 22);
-            this.textBox3.TabIndex = 3;
+            this.inputTextBox.Location = new System.Drawing.Point(13, 35);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(257, 22);
+            this.inputTextBox.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Location = new System.Drawing.Point(13, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 17);
             this.label1.TabIndex = 4;
@@ -78,7 +75,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(265, 60);
+            this.label2.Location = new System.Drawing.Point(337, 15);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 17);
             this.label2.TabIndex = 5;
@@ -98,41 +95,14 @@
             this.timeBox.TabStop = false;
             this.timeBox.Text = "Tijdsduur";
             // 
-            // newInput
+            // mileniaCount
             // 
-            this.newInput.Location = new System.Drawing.Point(12, 12);
-            this.newInput.Name = "newInput";
-            this.newInput.Size = new System.Drawing.Size(49, 26);
-            this.newInput.TabIndex = 7;
-            this.newInput.Text = "Nieuw";
-            this.newInput.UseVisualStyleBackColor = true;
-            // 
-            // yearCount
-            // 
-            this.yearCount.AutoSize = true;
-            this.yearCount.Location = new System.Drawing.Point(33, 71);
-            this.yearCount.Name = "yearCount";
-            this.yearCount.Size = new System.Drawing.Size(64, 17);
-            this.yearCount.TabIndex = 8;
-            this.yearCount.Text = "0 Jaaren";
-            // 
-            // dayCount
-            // 
-            this.dayCount.AutoSize = true;
-            this.dayCount.Location = new System.Drawing.Point(33, 108);
-            this.dayCount.Name = "dayCount";
-            this.dayCount.Size = new System.Drawing.Size(62, 17);
-            this.dayCount.TabIndex = 9;
-            this.dayCount.Text = "0 Dagen";
-            // 
-            // minuteCount
-            // 
-            this.minuteCount.AutoSize = true;
-            this.minuteCount.Location = new System.Drawing.Point(33, 139);
-            this.minuteCount.Name = "minuteCount";
-            this.minuteCount.Size = new System.Drawing.Size(70, 17);
-            this.minuteCount.TabIndex = 10;
-            this.minuteCount.Text = "0 Minuten";
+            this.mileniaCount.AutoSize = true;
+            this.mileniaCount.Location = new System.Drawing.Point(33, 36);
+            this.mileniaCount.Name = "mileniaCount";
+            this.mileniaCount.Size = new System.Drawing.Size(64, 17);
+            this.mileniaCount.TabIndex = 12;
+            this.mileniaCount.Text = "0 Milenia";
             // 
             // secondCount
             // 
@@ -143,26 +113,95 @@
             this.secondCount.TabIndex = 11;
             this.secondCount.Text = "0 Seconden";
             // 
-            // mileniaCount
+            // minuteCount
             // 
-            this.mileniaCount.AutoSize = true;
-            this.mileniaCount.Location = new System.Drawing.Point(33, 36);
-            this.mileniaCount.Name = "mileniaCount";
-            this.mileniaCount.Size = new System.Drawing.Size(64, 17);
-            this.mileniaCount.TabIndex = 12;
-            this.mileniaCount.Text = "0 Milenia";
+            this.minuteCount.AutoSize = true;
+            this.minuteCount.Location = new System.Drawing.Point(33, 139);
+            this.minuteCount.Name = "minuteCount";
+            this.minuteCount.Size = new System.Drawing.Size(70, 17);
+            this.minuteCount.TabIndex = 10;
+            this.minuteCount.Text = "0 Minuten";
+            // 
+            // dayCount
+            // 
+            this.dayCount.AutoSize = true;
+            this.dayCount.Location = new System.Drawing.Point(33, 108);
+            this.dayCount.Name = "dayCount";
+            this.dayCount.Size = new System.Drawing.Size(62, 17);
+            this.dayCount.TabIndex = 9;
+            this.dayCount.Text = "0 Dagen";
+            // 
+            // yearCount
+            // 
+            this.yearCount.AutoSize = true;
+            this.yearCount.Location = new System.Drawing.Point(33, 71);
+            this.yearCount.Name = "yearCount";
+            this.yearCount.Size = new System.Drawing.Size(64, 17);
+            this.yearCount.TabIndex = 8;
+            this.yearCount.Text = "0 Jaaren";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(340, 38);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(16, 17);
+            this.outputLabel.TabIndex = 7;
+            this.outputLabel.Text = "0";
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
+            // 
+            // forloopButton
+            // 
+            this.forloopButton.Location = new System.Drawing.Point(100, 63);
+            this.forloopButton.Name = "forloopButton";
+            this.forloopButton.Size = new System.Drawing.Size(82, 23);
+            this.forloopButton.TabIndex = 8;
+            this.forloopButton.Text = "For-Loop";
+            this.forloopButton.UseVisualStyleBackColor = true;
+            this.forloopButton.Click += new System.EventHandler(this.forloopButton_Click);
+            // 
+            // recursieButton
+            // 
+            this.recursieButton.Location = new System.Drawing.Point(188, 63);
+            this.recursieButton.Name = "recursieButton";
+            this.recursieButton.Size = new System.Drawing.Size(82, 23);
+            this.recursieButton.TabIndex = 9;
+            this.recursieButton.Text = "Recursie";
+            this.recursieButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Aantal Moves";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(340, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.outputLabel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 540);
-            this.Controls.Add(this.newInput);
+            this.ClientSize = new System.Drawing.Size(563, 540);
+            this.Controls.Add(this.recursieButton);
+            this.Controls.Add(this.forloopButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.timeBox);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.goButton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -176,8 +215,7 @@
         #endregion
 
         private System.Windows.Forms.Button goButton;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox inputTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox timeBox;
@@ -186,7 +224,11 @@
         private System.Windows.Forms.Label minuteCount;
         private System.Windows.Forms.Label dayCount;
         private System.Windows.Forms.Label yearCount;
-        private System.Windows.Forms.Button newInput;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button forloopButton;
+        private System.Windows.Forms.Button recursieButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- TVH/Form1.cs
+++ TVH/Form1.cs
@@ -16,5 +16,35 @@ namespace TVH
         {
             InitializeComponent();
         }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            int input;
+            ulong answer;
+            int.TryParse(inputTextBox.Text, out input);
+
+            answer = (ulong)(Math.Pow(2, input) - 1);
+            outputLabel.Text = string.Format("{0}", answer);
+
+        }
+
+        private void forloopButton_Click(object sender, EventArgs e)
+        {
+            double input;
+            double answer = 1;
+            double.TryParse(inputTextBox.Text, out input);
+
+            for(int i= 1; i < input; i++)
+            {
+                answer = answer * 2 + 1;
+            }
+            outputLabel.Text = string.Format("{0}", answer);
+        }
+
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
